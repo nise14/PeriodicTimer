0c94983b22698d50a17bce4574a6c0cf68069f08
Nicolas <nise14@htomail.com>
2024-02-27 12:43:09 -0500

    First commit

---
+++ PeriodTimerDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using PeriodicTimer timer = new(TimeSpan.FromMilliseconds(1000));
+var i = 0;
+
+while (await timer.WaitForNextTickAsync() && i < 5)
+{
+    Console.WriteLine($"Execution #{i + 1} ran at: {DateTime.Now:hh:mm:ss fff}");
+    i++;
+    timer.Period = TimeSpan.FromMilliseconds(i * 1000);
+    timer.Period = Timeout.InfiniteTimeSpan;
+}
+
+Console.WriteLine("We are done");
+Console.ReadLine();
